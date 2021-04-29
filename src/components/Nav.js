3b538904852185a50400ad71f551c8e2da17c763
Tom <demagny.t@hotmail.fr>
2021-04-29 19:40:11 +0200

    diff entre function et class

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,39 @@ import React from 'react';
 
 import Image from '../images/G4_FruityBio_1.png';
 
-class Nav extends React.Component{
+export default function Nav() {
+
+    return (
+        <React.Fragment>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-primary" style={{fontSize: '25px'}}>
+            <h1><img src={Image} style={{width: '40%'}} alt="logo G4"/></h1>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
+                    aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarColor01">
+              <ul className="mr-auto navbar-nav">
+                <li className="nav-item active">
+                  <a className="nav-link" href="/">Accueil
+                    <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/fruits">Nos Fruits</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/cart">Votre panier</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </React.Fragment>
+    )
+}
+
+
+/*class Nav extends React.Component{
 
   render(){
     return(
@@ -36,4 +68,5 @@ class Nav extends React.Component{
   }
 }
 
-export default Nav;
+export default Nav;  */
+
