b6c5779ca57d58d2ee1a5cf8abf40f1c52ab793e
mpreira <mpreira@me.com>
2021-04-29 14:59:14 +0200

    Plus Moins

--- src/components/Items.js
+++ src/components/Items.js
@@ -25,7 +25,7 @@ class Items extends React.Component{
             <h4 className="card-title">{ name }</h4>
             <img src={src} alt={name} style={{width:'33%'}} />
             <p className="card-text">Prix à l'unité: { price }€</p>
-            <p className="flex-row d-flex justify-content-center align-items-center"><i class="fas fa-plus"></i> <i class="fas fa-minus"></i> { quantity }</p>
+            <p className="flex-row d-flex justify-content-center align-items-center btn-group-horizontal"><button className="mr-2 btn btn-primary">+</button> { quantity } <button className="ml-2 btn btn-primary">-</button></p>
           </div>
         </div>
       </>
