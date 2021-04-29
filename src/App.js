16e8d4418032ca0e543a4538366cea21a2ffdaea
mpreira <mpreira@me.com>
2021-04-29 10:47:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import Message from './components/Message.js';
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      count: 10,
+    }
+  }
+
+  render(){
+    return(
+      <div className="App-header">
+        <p>Je suis App {this.state.count}</p>
+        <Message />
+      </div>
+      
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Message extends React.Component{
+
+  render(){
+    return(
+
+      <p>Je suis un message</p>
+
+    )
+  }
+}
+
+export default Message;
