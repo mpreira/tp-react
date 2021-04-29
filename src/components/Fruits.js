797879cd028fc970c83bbff5358350bdfb9bcf24
mpreira <mpreira@me.com>
2021-04-29 17:36:01 +0200

    Fix CSS panier/item

--- src/components/Fruits.js
+++ src/components/Fruits.js
@@ -14,7 +14,23 @@ class Fruits extends React.Component{
       <div className="container mt-4">
         
         <div className="d-flex">
-          <Items name="Pomme" src={Apple} price="0" style={{width:'30%'}} /> <Items name="Raisins" src={Grapes} price="0" style={{width:'30%'}} /> <Items name="Kiwi" src={Kiwi} price="0" style={{width:'30%'}} />
+          <div className="d-flex flex-column align-items-center">
+            <Items name="Pomme" src={Apple} price="0" style={{width:'30%'}} /> 
+            <button type="button" class="btn btn-secondary mt-2">Ajouter au panier</button> 
+          </div>
+          
+          <div className="d-flex flex-column align-items-center">
+            <Items name="Raisins" src={Grapes} price="0" style={{width:'30%'}} />
+            <button type="button" class="btn btn-secondary mt-2">Ajouter au panier</button>
+          </div>
+          
+          <div className="d-flex flex-column align-items-center">
+            <Items name="Kiwi" src={Kiwi} price="0" style={{width:'30%'}} />
+            <button type="button" class="btn btn-secondary mt-2">Ajouter au panier</button>
+          </div>
+          
+
+          
         </div>
         
       </div>
