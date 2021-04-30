9c66b8e42f44266fffc618c8871b80c25311dfd5
ohick <oliviahick1@gmail.com>
2021-04-30 10:45:30 +0200

    test panier

--- src/components/Fruits.js
+++ src/components/Fruits.js
@@ -5,27 +5,45 @@ import Apple from '../images/4.png';
 import Grapes from '../images/2.png';
 import Kiwi from '../images/3.png';
 
-class Fruits extends React.Component{
-
-  transfertShop(){
-    console.log('transferé');
+class Fruits extends React.Component {
+  transfertShop(fruit) {
+    console.log(fruit);
+    /* this.state.panier.push(fruit);
+    this.setState({ panier: this.state.panier });
+    console.log(this.state); */
   }
 
-  render(){
-
-    return(
-
+  render() {
+    return (
       <div className="container mt-4">
-        
         <div className="d-flex">
-          <Items panier={true} transfertShop={this.transfertShop.bind(this)} name="Pomme" src={Apple} price="3" style={{width:'30%'}} /> 
-          <Items panier={true} name="Raisins" src={Grapes} price="5" style={{width:'30%'}} /> 
-          <Items panier={true} name="Kiwi" src={Kiwi} price="1" style={{width:'30%'}} />
+          <Items
+            panier={true}
+            transfertShop={this.transfertShop.bind(this)}
+            name="Pomme"
+            src={Apple}
+            price="3"
+            style={{ width: '30%' }}
+          />
+          <Items
+            panier={true}
+            transfertShop={this.transfertShop.bind(this)}
+            name="Raisins"
+            src={Grapes}
+            price="5"
+            style={{ width: '30%' }}
+          />
+          <Items
+            panier={true}
+            transfertShop={this.transfertShop.bind(this)}
+            name="Kiwi"
+            src={Kiwi}
+            price="1"
+            style={{ width: '30%' }}
+          />
         </div>
-        
       </div>
-
-    )
+    );
   }
 }
 
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,83 +1,86 @@
 import React from 'react';
 
-class Items extends React.Component{
-  constructor(props){
+class Items extends React.Component {
+  constructor(props) {
     super(props);
 
     this.state = {
-      apple : '',
-      grapes : '',
-      kiwi : '',
-      quantity : 1,
-      prixFinal : parseInt(this.props.price) ,
-    }
+      apple: '',
+      grapes: '',
+      kiwi: '',
+      quantity: 1,
+      prixFinal: parseInt(this.props.price),
+    };
 
-    this.addToShop = this.addToShop.bind(this)
-    this.increment = this.increment.bind(this)
-    this.decrement = this.decrement.bind(this)
-    this.reset = this.reset.bind(this)
+    this.addToShop = this.addToShop.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
-  increment(){
-    const total = this.state.quantity +1
+  increment() {
+    const total = this.state.quantity + 1;
     this.setState({
-      quantity : total,
-      prixFinal: total * this.props.price
-    })
+      quantity: total,
+      prixFinal: total * this.props.price,
+    });
   }
-  decrement(){
-    const total = this.state.quantity -1
+  decrement() {
+    const total = this.state.quantity - 1;
 
-    if (this.state.quantity >0) {
+    if (this.state.quantity > 0) {
       this.setState({
-        quantity : total,
-        prixFinal: total * this.props.price
-      })
+        quantity: total,
+        prixFinal: total * this.props.price,
+      });
     }
   }
 
-  addToShop(){
-    this.props.transfertShop()
+  addToShop() {
+    this.props.transfertShop(this.props);
   }
 
-  reset(){
+  reset() {
     this.setState({
       quantity: 1,
-      prixFinal: this.props.price
-    })
+      prixFinal: this.props.price,
+    });
   }
 
-  render(){
+  render() {
     const { name, price, src } = this.props;
     const { quantity, prixFinal } = this.state;
     console.log(`rendered :  ${quantity}`);
 
-    return(
+    return (
+      <div className="card flex-fill">
+        <div className="card-body card_container">
+          <h4 className="card-title">{name}</h4>
 
+          <img src={src} alt={name} style={{ width: '50%' }} />
 
-        <div className="card flex-fill">
-          <div className="card-body card_container">
-            <h4 className="card-title">{ name }</h4>
-            
-            <img src={src} alt={name} style={{width:'50%'}} />
-
-            <p className="card-text">Prix à l'unité: { price }€</p>
-            <p className="flex-row d-flex justify-content-center align-items-center btn-group-horizontal">
-              <button onClick={this.increment} className="mr-2 btn btn-primary">+</button> 
-                {quantity } 
-              <button onClick={this.decrement} className="ml-2 btn btn-primary">-</button>
-            </p>
-            <p>Prix final : {prixFinal} euros</p>
-            <button onClick={this.reset} className="btn btn-warning mb-3">Reset</button>
-           {this.props.panier &&(
-              <button onClick={this.addToShop} className="btn btn-primary">Ajouter au panier</button>
-           )
-           }
-            
-          </div>
+          <p className="card-text">Prix à l'unité: {price}€</p>
+          <p className="flex-row d-flex justify-content-center align-items-center btn-group-horizontal">
+            <button onClick={this.increment} className="mr-2 btn btn-primary">
+              +
+            </button>
+            {quantity}
+            <button onClick={this.decrement} className="ml-2 btn btn-primary">
+              -
+            </button>
+          </p>
+          <p>Prix final : {prixFinal} euros</p>
+          <button onClick={this.reset} className="btn btn-warning mb-3">
+            Reset
+          </button>
+          {this.props.panier && (
+            <button onClick={this.addToShop} className="btn btn-primary">
+              Ajouter au panier
+            </button>
+          )}
         </div>
-
-    )
+      </div>
+    );
   }
 }
 
