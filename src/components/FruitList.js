cf4587a73536c3f3d493aa708f064c49985aac07
Tom <demagny.t@hotmail.fr>
2021-04-30 10:38:44 +0200

    Modifications from function to class

--- src/components/FruitList.js
+++ src/components/FruitList.js
@@ -1,21 +1,26 @@
 import React from "react";
 
-export const state = {
-        fruit : [
-            {
-                name: "pomme",
-                quantity : 0,
-                price : 2
-            },
-            {
-                name: "raisins",
-                quantity : 0,
-                price : 2
-            },
-            {
-                name: "kiwi",
-                quantity : 0,
-                price : 2
-            },
-        ]
+export default class FruitList extends React.Component {
+    constructor(props) {
+        super(props);
+            this.state = {
+                fruit : [
+                    {
+                        name: "pomme",
+                        quantity : 0,
+                        price : 2
+                    },
+                    {
+                        name: "raisins",
+                        quantity : 0,
+                        price : 2
+                    },
+                    {
+                        name: "kiwi",
+                        quantity : 0,
+                        price : 2
+                    },
+            ]
+        }
+    }
 }
